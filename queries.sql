d3a733414836b7d0673f2fbe311a83c032ad1edc
robertjallen <bobbidigi34@gmail.com>
2020-06-23 16:51:24 -0400

    add 3 results OrderId 10251

--- queries.sql
+++ queries.sql
@@ -4,9 +4,26 @@
               SELECT * from "Product"
               JOIN "category" ON "Product"."CategoryId" = "Category"."id" 
 
-              
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
--- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+      SELECT  "o"."ShipVia", "o"."Id", "o"."OrderDate", "s"."CompanyName"
 
+      from "Order" AS o
+
+      JOIN "Shipper" AS s 
+
+      ON "s"."Id" = "o"."ShipVia" 
+
+      where "o"."OrderDate" < '2012-08-12'
+
+-- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+    SELECT  "o"."OrderId", "o"."Quantity"
+
+    from "OrderDetail" as "o"
+
+
+    where "o"."OrderId" = 10251
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+

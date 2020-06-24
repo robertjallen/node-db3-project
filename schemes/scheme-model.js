8323ed0675e5b7cbfe915219225cdcdc4aba7fa8
robertjallen <bobbidigi34@gmail.com>
2020-06-23 20:39:21 -0400

    add remove

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -10,7 +10,22 @@ function findById(ID) {
 		.first("s.id", "s.scheme_name")
 }
 
+function findSteps(ID){
+  return db("steps as s").where("s.scheme_id", ID)
+  .innerJoin("schemes", "schemes.id", "s.scheme_id")
+  .select("s.step_number", "s.instructions", "schemes.scheme_name")
+  .orderBy("s.step_number")
+}
+
+function remove(ID){
+  return db('schemes')
+  .where({ id: ID })
+  .del()
+}
+
 module.exports = {
   find,
   findById,
+  findSteps,
+  remove,
 }
